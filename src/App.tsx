577e2f5ea9bdd6d39a4e11b6eb280e6465b08f22
Rimorin,Roman <roman.rimorin@freedompay.com>
2022-10-12 16:07:39 -0400

    added hear

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-function App() {
+import { ReactElement, FC } from 'react'
+
+import {
+  Header
+} from './components/General'
+
+const App: FC<any> = (): ReactElement => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="Test Header" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
